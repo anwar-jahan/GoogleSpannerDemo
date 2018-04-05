cd6ccb815e9044549f22a8e574f797ccb66f63b6
Anwar Jahan <anwar.jahan19@gmail.com>
2018-04-05 11:22:49 -0400

    add CORS issuse resolution

--- src/main/java/com/main/anwar/endpoint/HomeController.java
+++ src/main/java/com/main/anwar/endpoint/HomeController.java
@@ -1,10 +1,11 @@
 package com.main.anwar.endpoint;
-
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.main.anwar.endpoint.data.Door;
+import com.main.anwar.endpoint.data.DoorBO;
 import com.main.anwar.endpoint.service.DoorRepository;
 
 @RestController
@@ -27,13 +29,15 @@ public class HomeController {
 		return "Greetings from Spring Boot!";
 	}
 
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@RequestMapping(value = "/api/addDoor", method = {RequestMethod.POST, RequestMethod.GET})
 	@ResponseBody
-	public Door addDoor(@RequestParam("name") String name) {
+	public Door addDoor(@RequestBody(required = false) DoorBO door) {
 		log.info("Start: add method.");
-		return doorRepository.save(new Door(name));
+		return doorRepository.save(new Door(door.getName()));
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@RequestMapping(value="/api/getAllDoors", method=RequestMethod.GET)
 	@ResponseBody
 	public List<Door> getAllDoor() {
---
+++ src/main/java/com/main/anwar/endpoint/data/DoorBO.java
@@ -0,0 +1,16 @@
+package com.main.anwar.endpoint.data;
+
+public class DoorBO {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
